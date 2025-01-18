44a7116d74cfd4ef32b47aad5fcd90b31db37a53
Dav He <s991179@gmail.com>
2025-01-18 17:00:59 +0800

    feat: add time.sleep

--- concurrent_map_test2.go
+++ concurrent_map_test2.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"sync"
+	"time"
 
 	cmap "github.com/orcaman/concurrent-map/v2"
 )
@@ -13,12 +14,13 @@ func Concurrent_map_test2() {
 	m := cmap.New[int]()
 
 	var wg sync.WaitGroup
+	wg.Add(1000)
 
 	for i := 0; i < 1000; i++ {
-		wg.Add(1)
 		tmp := i
 		go func() {
 			defer wg.Done()
+			time.Sleep(100 * time.Millisecond)
 			m.Set(strconv.Itoa(tmp%10), tmp)
 		}()
 	}
